670145c099605ad05bad654f8f2087241a57bc6b
Habeebuddin Mir <habeebuddinmir@gmail.com>
2022-10-08 18:53:23 -0400

    employee classes created

---
+++ Assignment2/src/model/Employee.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.File;
+import java.util.Date;
+/**
+ *
+ * @author HABEEB
+ */
+public class Employee {
+    private String name;
+    private int employeeId;
+    private int age;
+    private String gender;
+    private Date startDate;
+    private String level;
+    private String teamInfo;
+    private String positionTitle;
+    private int cellPhoneNo;
+    private String emailAddress;
+    private File photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public void setPositionTitle(String positionTitle) {
+        this.positionTitle = positionTitle;
+    }
+
+    public int getCellPhoneNo() {
+        return cellPhoneNo;
+    }
+
+    public void setCellPhoneNo(int cellPhoneNo) {
+        this.cellPhoneNo = cellPhoneNo;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public File getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(File photo) {
+        this.photo = photo;
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+}
